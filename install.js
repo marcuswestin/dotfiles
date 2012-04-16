145dae3ecc7aed4df2f4fb494d1c0b3b9f4ac59b
Marcus Westin <marcus.westin@gmail.com>
2012-04-15 21:04:50 -0700

    bash profile changes and new install script

--- install.js
+++ install.js
@@ -1,7 +1,28 @@
+#!/usr/local/bin/node
+
 var fs = require('fs'),
 	path = require('path'),
 	home = process.argv[2],
-	filesPath = path.join(__dirname, 'files')
+
+
+var filesPath = path.join(__dirname, 'files')
+var files = fs.readdirSync(filesPath)
+for (var i=0, file; file = files[i]; i++) {
+	if (file[0] == '.' || file == 'install.js' || file == 'Makefile') { continue }
+	var linkPath = path.join(home, '.' + file),
+		filePath = path.join(filesPath, file)
+	if (path.existsSync(linkPath)) {
+		var linkStat = fs.lstatSync(linkPath)
+		if (linkStat.isSymbolicLink()) {
+			fs.unlinkSync(linkPath)
+		} else {
+			console.log(linkPath, 'already exists! Please remove it first. Skipping', file)
+			continue
+		}
+	}
+	console.log('.'+file+'!')
+	fs.symlinkSync(filePath, linkPath)
+}
 
 var files = fs.readdirSync(filesPath)
 for (var i=0, file; file = files[i]; i++) {
