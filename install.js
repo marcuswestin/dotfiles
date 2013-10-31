1f13d0d9fc62086f5445fd17c6a07cea0ba65ebc
Marcus Westin <narcvs@gmail.com>
2013-10-31 15:39:26 -0700

    Revert "better install script"
    This reverts commit 8e19cabd10cbe79c8a6b4bf87653a3e58a6c6ecb.

--- install.js
+++ install.js
@@ -10,20 +10,34 @@ var files = fs.readdirSync(filesPath)
 for (var i=0, file; file = files[i]; i++) {
 	if (file[0] == '.' || file == 'install.js' || file == 'Makefile') { continue }
 	var linkPath = path.join(home, '.' + file),
-		filePath = path.join(filesPath, file),
-		exists = false
-	try {
-		fs.readlinkSync(linkPath) // throws if nothing there
-		if (fs.lstatSync(linkPath).isSymbolicLink()) {
+		filePath = path.join(filesPath, file)
+	if (fs.readlinkSync(linkPath)) {
+		var linkStat = fs.lstatSync(linkPath)
+		if (linkStat.isSymbolicLink()) {
 			fs.unlinkSync(linkPath)
 		} else {
-			console.log(linkPath, 'already exists! Please remove it. ('+file+')')
-			process.exit(-1)
-		}
-	} catch(e) {
-		if (e.code != 'ENOENT' || e.errno != 34) {
-			throw e
+			console.log(linkPath, 'already exists! Please remove it first. Skipping', file)
+			continue
 		}
 	}
 	console.log('.'+file+'!')
+	fs.symlinkSync(filePath, linkPath)
+}
+
+var files = fs.readdirSync(filesPath)
+for (var i=0, file; file = files[i]; i++) {
+	if (file[0] == '.' || file == 'install.js' || file == 'Makefile') { continue }
+	var linkPath = path.join(home, '.' + file),
+		filePath = path.join(filesPath, file)
+	if (fs.readlinkSync(linkPath)) {
+		var linkStat = fs.lstatSync(linkPath)
+		if (linkStat.isSymbolicLink()) {
+			fs.unlinkSync(linkPath)
+		} else {
+			console.log(linkPath, 'already exists! Please remove it first. Skipping', file)
+			continue
+		}
+	}
+	console.log('.'+file+'!')
+	fs.symlinkSync(filePath, linkPath)
 }
