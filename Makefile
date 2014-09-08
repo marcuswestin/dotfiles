cb0025937488492f09e1b05199aa1bb9b60d1eb7
Marcus Westin <marcus.westin@gmail.com>
2014-09-08 10:18:57 -0400

    Better installation script by relying on hard links instead of syms, and only keeping the files I want in here (E.g not getting files-Sublime-User packed with stuff from automatic package installs

--- Makefile
+++ Makefile
@@ -2,7 +2,4 @@ install: *
 	node install.js ~
 	git config --global core.excludesfile ~/.gitignore
 	git config --global color.ui true
-	mkdir -p ~/bin
-	if [ ! -f ~/bin/subl ]; then ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl ~/bin/subl; fi;
-	rm -rf ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
-	ln -snf `pwd`/Sublime-User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+	if [ ! -f /usr/local/bin/subl ]; then ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl; fi;
--- install.js
+++ install.js
@@ -4,27 +4,33 @@ var fs = require('fs'),
 	path = require('path'),
 	home = process.argv[2]
 
+processDir('files-Home', home+'/.')
+processDir('files-Sublime-User', home+'/Library/Application Support/Sublime Text 3/Packages/User/')
 
-var filesPath = path.join(__dirname, 'files')
-var files = fs.readdirSync(filesPath)
-for (var i=0, file; file = files[i]; i++) {
-	if (file[0] == '.' || file == 'install.js' || file == 'Makefile') { continue }
-	var linkPath = path.join(home, '.' + file),
-		filePath = path.join(filesPath, file),
-		exists = false
-	try {
-		fs.readlinkSync(linkPath) // throws if nothing there
-		if (fs.lstatSync(linkPath).isSymbolicLink()) {
-			fs.unlinkSync(linkPath)
-		} else {
-			console.log(linkPath, 'already exists! Please remove it. ('+file+')')
-			process.exit(-1)
+function processDir(dirname, linkPrefix) {
+	var files = fs.readdirSync(path.join(__dirname, dirname))
+	for (var i=0, file; file = files[i]; i++) {
+		if (file[0] == '.') { continue }
+		var linkPath = linkPrefix + file,
+			linkPathHuman = linkPath.replace(home, '~').replace(/ /g, '\\ '),
+			filePath = path.join(dirname, file),
+			fileStat = fs.statSync(filePath),
+			linkStat
+		
+		console.log('\t',linkPathHuman)
+		
+		try {
+			var linkStat = fs.statSync(linkPath) // throws if nothing there
+		} catch(e) {}
+		if (linkStat) {
+			if (linkStat.ino = fileStat.ino) {
+				fs.unlinkSync(linkPath)
+			} else {
+				console.log(linkPathHuman, 'already exists! Please remove it.')
+				process.exit(-1)
+			}
 		}
-	} catch(e) {
-		if (e.code != 'ENOENT' || e.errno != 34) {
-			// throw e
-		}
+		
+		fs.linkSync(filePath, linkPath)
 	}
-	console.log('.'+file+'!')
-	fs.symlinkSync(filePath, linkPath)
 }
